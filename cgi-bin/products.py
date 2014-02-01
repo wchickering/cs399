dfa818845afaaa6fb04661671249cf27a90c9aef
Jamie Irvine <jirvine@cs.stanford.edu>
2014-02-01 03:45:59 -0800

    Cleaned up

---
+++ cgi-bin/products.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import sys; sys.path.insert(0, 'lib') # this line is necessary for the rest
+import os                             # of the imports to work!
+
+import json
+import web
+import sqlitedb
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+import cgi
+import time
+import pprint
+
+###########################################################################################
+##########################DO NOT CHANGE ANYTHING ABOVE THIS LINE!##########################
+###########################################################################################
+
+######################BEGIN HELPER METHODS######################
+
+# helper method to convert times from database (which will return a string)
+# into datetime objects. This will allow you to compare times correctly (using
+# ==, !=, <, >, etc.) instead of lexicographically as strings.
+
+# Sample use:
+# current_time = string_to_time(sqlitedb.getTime())
+
+def string_to_time(date_str):
+    return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+# helper method to render a template in the templates/ directory
+#
+# `template_name': name of template file to render
+#
+# `**context': a dictionary of variable names mapped to values
+# that is passed to Jinja2's templating engine
+#
+# See curr_time's `GET' method for sample usage
+#
+# WARNING: DO NOT CHANGE THIS METHOD
+def render_template(template_name, **context):
+    extensions = context.pop('extensions', [])
+    globals = context.pop('globals', {})
+
+    jinja_env = Environment(autoescape=True,
+            loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+            extensions=extensions,
+            )
+    jinja_env.globals.update(globals)
+
+    web.header('Content-Type','text/html; charset=utf-8', unique=True)
+
+    return jinja_env.get_template(template_name).render(context)
+
+#####################END HELPER METHODS#####################
+
+urls = ( '/leftorright', 'left_or_right',
+        )
+
+class left_or_right:
+   def GET(self):
+       return render_template('index.html');
+
+   def POST(self):
+       post_params = web.input()
+       productId = post_params['productId']
+       product = sqlitedb.getNextProduct(productId)
+       json_obj = json.dumps(product)
+       return json_obj
+
+###########################################################################################
+##########################DO NOT CHANGE ANYTHING BELOW THIS LINE!##########################
+###########################################################################################
+
+if __name__ == '__main__':
+    web.internalerror = web.debugerror
+    app = web.application(urls, globals())
+    app.add_processor(web.loadhook(sqlitedb.enforceForeignKey))
+    app.run()
