bc097611710ef185562c89480dac6efcc6154d0d
Bill Chickering <chickering@cs.stanford.edu>
2014-02-16 14:05:24 -0800

    Add multiprocessing to bdales_recs.py.

--- bdales/bdales_recs.py
+++ bdales/bdales_recs.py
@@ -1,14 +1,17 @@
 #!/usr/local/bin/python
 
+import multiprocessing as mp
+import time
 import urllib2
-from bs4 import BeautifulSoup, NavigableString
+from bs4 import BeautifulSoup
 import sqlite3
 import sys
+import selenium
 from selenium import webdriver
 
 # params
+numWorkers = 4
 productUrlTemplate = 'http://www1.bloomingdales.com/shop/product/?ID=%d'
-cycleDriverInterval = 100
 loadTimeout = 30
 
 # db params
@@ -24,12 +27,14 @@ selectProductsStmt =\
 selectProductStmt = 'SELECT * FROM Products WHERE Id = :Id'
 updateProductStmt = 'UPDATE Products SET HaveRecs = 1 WHERE Id = :Id'
 
+
 def getWebDriver():
     profile = webdriver.FirefoxProfile()
     profile.set_preference('permissions.default.image', 2)
     driver = webdriver.Firefox(firefox_profile=profile)
     driver.set_page_load_timeout(loadTimeout)
     return driver
+
 
 def processRecommend(id1, productTag, db_curs):
     # process productTag
@@ -43,38 +48,72 @@ def processRecommend(id1, productTag, db_curs):
     if db_curs.fetchone():
         return 0
     # check db for product
-    print 'Id2 = %d' % id2
+    #print 'Id2 = %d' % id2
     db_curs.execute(selectProductStmt, (id2,))
     if not db_curs.fetchone():
-        print >> sys.stderr, 'WARNING: Product not in DB.'
+        #print >> sys.stderr, 'WARNING: Product not in DB.'
         return 0
     # insert new recommendation
     db_curs.execute(insertRecommendStmt, (id1, id2))
     return 1
 
+
 def getRecommends(driver, productId, db_curs):
-    print 'productId = %d' % productId
+    #print 'productId = %d' % productId
+    insertCnt = 0
     productUrl = productUrlTemplate % productId
     try:
         # fetch html
-        print 'Opening url: %s' % productUrl
+        #print 'Opening url: %s' % productUrl
         driver.get(productUrl)
         html = driver.page_source
         soup = BeautifulSoup(html)
-        insertCnt = 0
         for productTag in\
             soup.findAll('div', {'class': 'productThumbnail showQuickView'}):
             insertCnt +=\
                 processRecommend(productId, productTag, db_curs)
         if insertCnt == 0:
-            print 'No recommends found.'
+            #print 'No recommends found.'
+            pass
         db_curs.execute(updateProductStmt, (productId,))
     except selenium.common.exceptions.TimeoutException:
-        print >> sys.stderr,\
-            'WARNING: Timed out waiting for page load.'
+        #print >> sys.stderr,\
+        #    'WARNING: Timed out waiting for page load.'
+        pass
     return insertCnt
 
-def main():
+
+def worker(master_pipe, worker_pipe):
+    master_pipe.close()
+
+    # connect to db
+    db_conn = sqlite3.connect(db_fname)
+    with db_conn:
+        db_curs = db_conn.cursor()
+
+        driver = getWebDriver()
+
+        insertCnt = 0
+        while True:
+            try:
+                productId = worker_pipe.recv()
+                insertCnt += getRecommends(driver, productId, db_curs)
+                db_conn.commit()
+            except EOFError:
+                break
+
+        driver.quit()
+
+    #print 'Inserted %d new recommendations.' % insertCnt
+
+    worker_pipe.close()
+        
+
+def master(pipes):
+    # close worker ends of pipes
+    for w in range(numWorkers):
+        pipes[w][1].close()
+
     # connect to db
     db_conn = sqlite3.connect(db_fname)
     with db_conn:
@@ -83,19 +122,40 @@ def main():
         db_curs.execute(createRecommendsTableStmt)
         # get item ids
         db_curs.execute(selectProductsStmt)
-        productIds = [row[0] for row in db_curs.fetchall()]
 
-        driver = getWebDriver()
+        # parsel out work
+        iter=0
+        for row in db_curs.fetchall():
+            productId = row[0]
+            workerIdx = iter % numWorkers
+            pipes[workerIdx][0].send(productId)
+            iter += 1
 
-        insertCnt = 0
-        for productId in productIds:
-            insertCnt += getRecommends(driver, productId, db_curs)
-            db_conn.commit()
-            if insertCnt % cycleDriverInterval == 0:
-                driver.quit()
-                driver = getWebDriver()
+    # close master ends of pipes
+    for w in range(numWorkers):
+        pipes[w][0].close()
 
-    print 'Inserted %d new recommendations.' % insertCnt
+
+def main():
+    # create pipes
+    pipes = []
+    for w in range(numWorkers):
+        master_pipe, worker_pipe = mp.Pipe()
+        pipes.append((master_pipe, worker_pipe))
+
+    # create worker processes
+    workers = []
+    for w in range(numWorkers):
+        w = mp.Process(target=worker, args=(pipes[w][0], pipes[w][1]))
+        workers.append(w)
+
+    # start worker processes
+    for w in range(numWorkers):
+        workers[w].start()
+
+    # delegate work
+    master(pipes)
+
 
 if __name__ == '__main__':
     main()
