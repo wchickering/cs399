955252e2487bf1376de13caab148658b3d3a360a
Bill Chickering <chickering@cs.stanford.edu>
2014-03-07 13:49:02 -0800

    Add NumUsersCommon to aggregateResults.py.

--- amazon/aggregateResults.py
+++ amazon/aggregateResults.py
@@ -98,11 +98,13 @@ def main():
     # Aggregate results
     avgVariance = []
     avgError = []
+    avgCommonUsers = []
     avgLess = []
     maxVariance = []
     maxFilenames = []
     varVariance = []
     varError = []
+    varCommonUsers = []
     varLess = []
     finished = False
     j = 0
@@ -110,6 +112,7 @@ def main():
         # Compute averages
         totalVar = 0
         totalErr = 0
+        totalCommonUsers = 0
         totalLess = 0
         maxVar = 0
         maxFilename = None
@@ -120,6 +123,7 @@ def main():
                     maxFilename = filenames[i]
                 totalVar += variances[i][j]
                 totalErr += errors[i][j]
+                totalCommonUsers += int(data[i][j][numUsersComIdx])
                 if data[i][j][numUsers1Idx] < data[i][j][numUsers2Idx]:
                     totalLess += int(data[i][j][numUsers1Idx])
                 else:
@@ -133,6 +137,7 @@ def main():
         maxFilenames.append(maxFilename)
         avgVariance.append(totalVar/len(variances))
         avgError.append(totalErr/len(errors))
+        avgCommonUsers.append(totalCommonUsers/len(data))
         avgLess.append(totalLess/len(data))
         # Compute variance of variance
         totalVar = 0
@@ -144,6 +149,12 @@ def main():
         for i in range(len(errors)):
             totalErr += (errors[i][j] - avgError[-1])**2
         varError.append(totalErr/len(errors))
+        # Compute variance of common users
+        totalCommonUsers = 0
+        for i in range(len(data)):
+            totalCommonUsers +=\
+                (int(data[i][j][numUsersComIdx]) - avgCommonUsers[-1])**2
+        varCommonUsers.append(totalCommonUsers/len(data))
         # Compute variance of less reviews
         totalLess = 0
         for i in range(len(data)):
@@ -165,6 +176,7 @@ def main():
             writer.writerow([numUsers[i],
                              avgVariance[i], varVariance[i],
                              avgError[i], varError[i],
+                             avgCommonUsers[i], varCommonUsers[i],
                              avgLess[i], varLess[i],
                              maxVariance[i], maxFilenames[i]])
 
