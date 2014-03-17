ff4d32091f678e39d4dbd0e767d0ddd44b3fa442
Bill Chickering <chickering@cs.stanford.edu>
2014-03-16 21:25:53 -0700

    Generalize ratingsVsSim.py.

--- amazon/ratingsVsSim.py
+++ amazon/ratingsVsSim.py
@@ -1,13 +1,15 @@
 #!/usr/local/bin/python
 
 """
-Generates several histograms, one for each unique combination of rating values
+Generates several histograms, each corresponding to a pair of rating ranges
 assigned to a pair of products by a common reviewer. The histograms are binned
 by the ultimate similarity score for the pair of products.
 """
 
 from optparse import OptionParser
 import sqlite3
+import numpy
+import collections
 import csv
 import os
 import sys
@@ -15,9 +17,7 @@ import sys
 import similarity
 
 # params
-minRating = 1
-maxRating = 5
-outputFileTemplate = '%s_%d_%d.csv'
+outputFileTemplateTemplate = '%s_%s_%%0.3f_%%0.3f.csv'
 displayInterval = 10000
 
 # db params
@@ -39,23 +39,83 @@ def getParser(usage=None):
     parser.add_option('-c', '--cosineFunc', dest='cosineFunc',
         default='prefSim',
         help=('Similarity function to use: "prefSim" (default), "randSim", '
-              '"prefSimAlt1", or "randSimAlt1".'),
-        metavar='FUNCNAME')
+              '"prefSimAlt1", or "randSimAlt1".'), metavar='FUNCNAME')
+    parser.add_option('--minRating', type='float', dest='minRating',
+        default=0.5, help='Minimum rating.', metavar='FLOAT')
+    parser.add_option('--maxRating', type='float', dest='maxRating',
+        default=5.5, help='Maximum rating.', metavar='FLOAT')
+    parser.add_option('--stepRating', type='float', dest='stepRating',
+        default=1.0, help='Step rating.', metavar='FLOAT')
+    parser.add_option('-t', '--ratingType', dest='ratingType', default='raw',
+              help='Rating type to use: "raw" (default), or "adjusted"',
+              metavar='FUNCNAME')
     return parser
 
-def initHistograms(minRating, maxRating, numBins):
-    histograms = {}
-    for rating1 in range(minRating, maxRating + 1):
-        for rating2 in range(rating1, maxRating + 1):
-            histograms[(rating1, rating2)] = [0]*numBins
-    return histograms
+class Histograms(collections.MutableMapping):
+    """A dictionary of histograms indexed by rating pairs."""
+    def __init__(self, minRating, maxRating, stepRating, numBins):
+        self.minRating = minRating
+        self.maxRating = maxRating
+        self.stepRating = stepRating
+        self.store = {}
+        # initialize histograms
+        for key in self:
+            self[key] = [0]*numBins
+        # include null histogram to return for bad keys
+        self[(self.maxRating, self.maxRating)] = None
+
+    def __keytransform__(self, key):
+        rating1 = min(key[0], key[1])
+        rating2 = max(key[0], key[1])
+        if rating1 < self.minRating or rating2 >= self.maxRating:
+            return -1
+        idx = 0
+        p = self.minRating
+        while p < self.maxRating:
+            q = p
+            while q < self.maxRating:
+                if rating1 >= p and rating1 < p + self.stepRating and\
+                   rating2 >= q and rating2 < q + self.stepRating:
+                    return idx
+                idx += 1
+                q += self.stepRating
+            p += self.stepRating
+        raise Exception("Unreachable code.")
+
+    def __getitem__(self, key):
+        return self.store[self.__keytransform__(key)]
+
+    def __setitem__(self, key, value):
+        self.store[self.__keytransform__(key)] = value
+
+    def __delitem__(self, key):
+        del self.store[self.__keytransform__(key)]
+
+    def __len__(self):
+        return len(self.store)
+
+    def __iter__(self):
+        self.current = [self.minRating, self.minRating]
+        return self
+
+    def next(self):
+        if self.current[0] >= self.maxRating - self.stepRating and\
+           self.current[1] >= self.maxRating:
+            raise StopIteration
+        elif self.current[1] >= self.maxRating:
+           self.current[0] += self.stepRating
+           self.current[1] = self.current[0] + self.stepRating
+        else:
+           self.current[1] += self.stepRating
+        return (self.current[0], self.current[1] - self.stepRating)
 
 def incrementBin(bins, cosineSim):
     bins[int(round((1.0 + cosineSim)*(len(bins) - 1)/2.0))] += 1
 
-def makeHistograms(db_conn, inputfile, cosineFunc, histograms):
+def fillHistograms(db_conn, inputfile, cosineFunc, ratingType, histograms):
     db_curs = db_conn.cursor()
     count = 0
+    bad_keys = 0
     for line in inputfile:
         tokens = line.split(',')
         productId1 = tokens[0].strip()
@@ -67,6 +127,9 @@ def makeHistograms(db_conn, inputfile, cosineFunc, histograms):
         reviews2 = [row for row in db_curs.fetchall()]
         # compute cosine similarity using the provided function.
         cosineSim, numUserCommon = cosineFunc(reviews1, reviews2)
+        # compute product biases
+        bias1 = numpy.mean([review[2] for review in reviews1])
+        bias2 = numpy.mean([review[2] for review in reviews2])
         # update histograms
         i = 0
         j = 0
@@ -84,31 +147,38 @@ def makeHistograms(db_conn, inputfile, cosineFunc, histograms):
                     i += 1
                     j += 1
                     continue # ignore duplicate reviews
-                count += 1
-                rating1 = reviews1[i][3]
-                rating2 = reviews2[j][3]
-                minRating = min(rating1, rating2)
-                maxRating = max(rating1, rating2)
-                histogram = histograms[(minRating, maxRating)]
-                incrementBin(histogram, cosineSim)
+                if ratingType == 'adjusted':
+                    rating1 = reviews1[i][2] - bias1
+                    rating2 = reviews2[j][2] - bias2
+                else:
+                    rating1 = reviews1[i][3]
+                    rating2 = reviews2[j][3]
+                histogram = histograms[(rating1, rating2)]
+                if not histogram:
+                    bad_keys += 1
+                else:
+                    count += 1
+                    incrementBin(histogram, cosineSim)
+                    if count % displayInterval == 0:
+                        print '%d rating pairs processed' % count
                 i += 1
                 j += 1
-                if count % displayInterval == 0:
-                    print '%d rating pairs processed' % count
-    return histograms
+    print '%d Bad Keys.' % bad_keys
 
-def printHistograms(outputDir, simType, histograms):
-    for rating1 in range(minRating, maxRating + 1):
-        for rating2 in range(rating1, maxRating + 1):
-            outputFileName = os.path.join(outputDir,
-                outputFileTemplate % (simType, rating1, rating2))
-            with open(outputFileName, 'wb') as csvfile:
-                writer = csv.writer(csvfile)
-                histogram = histograms[(rating1, rating2)]
-                for i in range(len(histogram)):
-                    writer.writerow([
-                        round((i + 0.5)*2.0/len(histogram) - 1.0, 3),
-                        histogram[i]])
+def printHistograms(outputDir, outputFileTemplate, histograms):
+    for key in histograms:
+        rating1 = key[0]
+        rating2 = key[1]
+        histogram = histograms[(rating1, rating2)]
+        outputFileName = os.path.join(outputDir,
+            outputFileTemplate % (rating1, rating2))
+        print 'Writing to %s . . .' % outputFileName
+        with open(outputFileName, 'wb') as csvfile:
+            writer = csv.writer(csvfile)
+            for i in range(len(histogram)):
+                writer.writerow([
+                    round((i + 0.5)*2.0/len(histogram) - 1.0, 3),
+                    histogram[i]])
 
 def main():
     # Parse options
@@ -140,13 +210,17 @@ def main():
     inputfile = open(inputFileName, 'r')
 
     # Initialize histograms
-    histograms = initHistograms(minRating, maxRating, options.numBins)
+    histograms = Histograms(options.minRating, options.maxRating,
+                            options.stepRating, options.numBins)
 
     # Generate histogram data
-    histograms = makeHistograms(db_conn, inputfile, cosineFunc, histograms)
+    fillHistograms(db_conn, inputfile, cosineFunc, options.ratingType,
+                   histograms)
 
     # Output histograms
-    printHistograms(options.outputDir, options.cosineFunc, histograms)
+    outputFileTemplate = outputFileTemplateTemplate %\
+                         (options.cosineFunc, options.ratingType)
+    printHistograms(options.outputDir, outputFileTemplate, histograms)
 
 if __name__ == '__main__':
     main()
