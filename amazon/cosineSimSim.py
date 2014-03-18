53ab7337d00520563fbffddcdb8866fd21bd394b
Bill Chickering <chickering@cs.stanford.edu>
2014-03-18 02:25:36 -0700

    Implement multivariate gaussian prior in cosineSimSim.py.

--- amazon/cosineSimSim.py
+++ amazon/cosineSimSim.py
@@ -5,29 +5,34 @@ import os
 import sys
 import random
 import math
+import numpy as np
 
 def getParser(usage=None):
     parser = OptionParser(usage=usage)
     parser.add_option('-r', '--randomSeed', type='int', dest='randomSeed',
         default=0, help='Seed for random module.', metavar='NUM')
     parser.add_option('-d', '--dimensions', type='int', dest='dimensions',
-        default=100, help='Number of dimensions.', metavar='NUM')
-    parser.add_option('-s', '--sigma', type='float', dest='sigma',
-        default=1.0, help='Standard deviation of guassian distribution.',
-        metavar='FLOAT')
+        default=10000, help='Number of dimensions.', metavar='NUM')
+    parser.add_option('--sigmaXX', type='float', dest='sigmaXX', default=1.0,
+       help=('Diagonal component of covariance matrix for multivariate '
+             'Gaussian distribution prior for ratings.'), metavar='FLOAT')
+    parser.add_option('--sigmaXY', type='float', dest='sigmaXY', default=0.3,
+       help=('Off-diagonal component of covariance matrix for multivariate '
+             'Gaussian distribution prior for ratings.'), metavar='FLOAT')
     return parser
 
-def cosineSimSim(dimensions, sigma, mu1, mu2):
+def cosineSimSim(dimensions, mu, sigma):
+    assert(sigma.shape == (2, 2))
+    assert(mu.shape == (2,))
     innerProd = 0
     var1 = 0
     var2 = 0
     count = 0
     for i in range(dimensions):
-        elem1 = random.gauss(mu1, sigma)
-        elem2 = random.gauss(mu2, sigma)
-        innerProd += elem1*elem2
-        var1 += elem1**2
-        var2 += elem2**2
+        elements = np.random.multivariate_normal(mu, sigma)
+        innerProd += elements[0]*elements[1]
+        var1 += elements[0]**2
+        var2 += elements[1]**2
     return innerProd/(math.sqrt(var1)*math.sqrt(var2))
 
 def main():
@@ -44,8 +49,13 @@ def main():
     # Seed random module
     random.seed(options.randomSeed)
 
+    # Form mu vector and sigma matrix
+    mu = np.array([rating1, rating2])
+    sigma = np.array([[options.sigmaXX, options.sigmaXY],
+                      [options.sigmaXY, options.sigmaXX]])
+
     # Run simulation
-    simsim = cosineSimSim(options.dimensions, options.sigma, rating1, rating2)
+    simsim = cosineSimSim(options.dimensions, mu, sigma)
 
     # Output
     print simsim
