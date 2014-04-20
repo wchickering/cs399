35b730c5cf38e5e18a733aae12551c2bdec73e9e
Bill Chickering <chickering@cs.stanford.edu>
2014-04-19 21:22:30 -0700

    Add top words to displayModel.py.

--- common/displayModel.py
+++ common/displayModel.py
@@ -11,6 +11,7 @@ from SessionTranslator import SessionTranslator
 
 # params
 imgsrcTemplate = 'images/%s.jpg'
+topnWords = 10
 
 def getParser(usage=None):
     parser = OptionParser(usage=usage)
@@ -22,6 +23,8 @@ def getParser(usage=None):
         help='Dictionary that maps itemids to tokens.', metavar='FILE')
     parser.add_option('-n', '--topn', type='int', dest='topn', default=10,
         help='Number of items per topic to print.', metavar='NUM')
+    parser.add_option('--tfidf', dest='tfidf', default=None,
+        help='File containing pickled TF-IDF scores.', metavar='FILE')
     return parser
 
 def getItemTopics(dictionary, model, item):
@@ -40,15 +43,22 @@ def getItemTopics(dictionary, model, item):
             p_item_given_topic[topic]*p_topic[topic]/p_item
     return [(ind, x) for ind, x in enumerate(p_topic_given_item)]
 
-def genHtml(dictionary, model, translator, topn):
+def genHtml(dictionary, model, translator, topn, tfidf=None):
     print '<html lang="en" debug="true">'
     print '<head><title>Display LDA Topics</title></head>'
     print '<body>'
     print '<p>model.alpha =', model.alpha, '</p>'
     # print top N items from each topic
     for topic in range(model.num_topics):
-        item_dist = model.show_topic(topic, topn=topn)
         print '<hr><div><b>Topic: %d</b>' % topic
+        if tfidf is not None:
+            print '<p>Top words for topic %d</p>' % topic
+            print '<ul>'
+            for i in range(topnWords):
+                print '<li>%s : %.3f</li>' %\
+                    (tfidf[topic][i][0], tfidf[topic][i][1])
+            print '</ul>'
+        item_dist = model.show_topic(topic, topn=topn)
         items = [pair[1] for pair in item_dist]
         descriptions = translator.sessionToDesc(items)
         for i in range(len(items)):
@@ -88,8 +98,19 @@ def main():
     # get translator
     translator = SessionTranslator(options.dbname)
 
+    # get tf-idf scores
+    if options.tfidf is not None:
+        with open(options.tfidf, 'r') as f:
+            try:
+                tfidf = pickle.load(f)
+            except:
+                print >> sys.stderr, 'ERROR: Failed to parse tfidf file.'
+                return
+    else:
+        tfidf = None
+
     # generate html document
-    genHtml(dictionary, model, translator, options.topn)
+    genHtml(dictionary, model, translator, options.topn, tfidf=tfidf)
 
 if __name__ == '__main__':
     main()
