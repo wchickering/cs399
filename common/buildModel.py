f1eb93a9c08bd430a77c198068daf7e6cb5aae85
Bill Chickering <chickering@cs.stanford.edu>
2014-04-26 13:49:44 -0700

    Refactor buildModel.py.

--- common/buildModel.py
+++ common/buildModel.py
@@ -7,53 +7,44 @@ Construct and save LDA model using Radim Rehurek's gensim module.
 from gensim import corpora
 from gensim.models import ldamodel
 from optparse import OptionParser
-import logging
 import pickle
 import csv
 
 def getParser(usage=None):
     parser = OptionParser(usage=usage)
-    parser.add_option('-f', '--file', dest='filename',
-        default='data/trainSessions.csv',
+    parser.add_option('-f', '--file', dest='filename', default=None,
         help=('CSV file containing training data. Each line consists of tokens '
               'liked in a single session.'), metavar='FILE')
-    parser.add_option('-d', '--dict-fname-in', dest='dict_fname_in',
-        default='data/tokens.csv',
-        help='File containing all tokens in the vocabulary.', metavar='FILE')
-    parser.add_option('--dict-fname-out', dest='dict_fname_out',
-        default='data/tokens.dict',
-        help='Save file for dictionary that maps itemids to tokens.',
-        metavar='FILE')
     parser.add_option('-l', '--lda-file', dest='lda_filename',
-        default='data/lda.pickle', help='Save file for LDA model.',
+        default='lda.pickle', help='Save file for LDA model.',
         metavar='FILE')
     parser.add_option('-t', '--num-topics', dest='num_topics', type='int',
-        default=100, help='Number of topics in LDA model.', metavar='NUM')
+        default=10, help='Number of topics in LDA model.', metavar='NUM')
     parser.add_option('-p', '--passes', dest='passes', type='int', default=1,
         help='Number of passes for training LDA model.', metavar='NUM')
     parser.add_option('--alpha', dest='alpha', default='symmetric',
         help='Alpha param for gensim.LdaModel().', metavar='VAL')
     return parser
 
-def buildDictionary(fname_in, fname_out):
-    logging.info('Building dictionary from %s.' % fname_in)
-    with open(fname_in) as csvIn:
+def getDictFromDocs(filename):
+    termDict = {}
+    with open(filename) as csvIn:
         reader = csv.reader(csvIn)
-        records = [record for record in reader]
-        dictionary = corpora.Dictionary(records)
-        logging.info('Saving dictionary to %s.' % fname_out)
-        dictionary.save_as_text(fname_out)
-    return dictionary
+        for record in reader:
+            for term in record:
+                termDict[term] = 1
+    terms = [[term] for term in termDict.keys()]
+    return corpora.Dictionary(terms)
             
-def getCorpus(dictionary, filename):
-    logging.info('Building training corpus from %s.' % filename)
+def getCorpusFromDocs(filename, dictionary):
     with open(filename) as csvIn:
         reader = csv.reader(csvIn)
         records = [record for record in reader]
-    return [dictionary.doc2bow(record) for record in records]
+    corpus = [dictionary.doc2bow(record) for record in records]
+    return corpus
 
-def buildLDA(dictionary, corpus, num_topics, passes, alpha):
-    lda = ldamodel.LdaModel(corpus=corpus,
+def buildModel(corpus, dictionary, num_topics, passes, alpha):
+    model = ldamodel.LdaModel(corpus=corpus,
                             num_topics=num_topics,
                             id2word=dictionary,
                             chunksize=100,
@@ -63,29 +54,30 @@ def buildLDA(dictionary, corpus, num_topics, passes, alpha):
                             eta=None,
                             decay=0.5,
                             eval_every=10)
-    return lda
+    return model
 
 def main():
-    # Setup logging
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
-                        level=logging.INFO)
-
     # Parse options
     usage = 'Usage: %prog [options]'
-    parser = getParser(usaage=usage)
+    parser = getParser(usage=usage)
     (options, args) = parser.parse_args()
 
-    # Construct dictionary
-    dictionary = buildDictionary(options.dict_fname_in, options.dict_fname_out)
+    # Build dictionary
+    print 'Building dictionary from %s. . .' % options.filename
+    dictionary = getDictFromDocs(options.filename)
 
     # Construct corpus
-    corpus = getCorpus(dictionary, options.filename)
+    print 'Building corpus from %s. . .' % options.filename
+    corpus = getCorpusFromDocs(options.filename, dictionary)
 
-    # Construct LDA Model
-    lda = buildLDA(dictionary, corpus, options.num_topics, options.passes,
-                   options.alpha)
-    logging.info('Saving LDA model to %s.' % options.lda_filename)
-    pickle.dump(lda, open(options.lda_filename, 'w'))
+    # Train LDA Model
+    print 'Training model. . .'
+    model = buildModel(corpus, dictionary, options.num_topics, options.passes,
+                       options.alpha)
+
+    # Save LDA Model
+    print 'Saving model to %s. . .' % options.lda_filename
+    pickle.dump(model, open(options.lda_filename, 'w'))
 
 if __name__ == '__main__':
     main()
