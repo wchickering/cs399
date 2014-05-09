b29de8c123a8066ac41ebcc5b88cc8c345813aa3
Jamie Irvine <jirvine@cs.stanford.edu>
2014-05-09 14:25:56 -0700

    Refactor tfidf item-item connecting to own script: predictEdgesTfidf.py

--- common/predictEdges.py
+++ common/predictEdges.py
@@ -3,17 +3,10 @@
 """
 Predicts the edges across two LDA/LSI model.
 """
-
-from stemming.porter2 import stem
 from optparse import OptionParser
-from collections import defaultdict
-from Queue import PriorityQueue
 import pickle
 import os
 import sys
-import sqlite3
-import string
-import math
 import numpy as np
 
 # local modules
@@ -21,25 +14,12 @@ import LDA_util as lda
 import LSI_util as lsi
 from KNNSearchEngine import KNNSearchEngine
 
-selectDescriptionStmt = 'SELECT Description FROM Products WHERE Id = :Id'
-displayInterval = 100
-
 def getParser(usage=None):
     parser = OptionParser(usage=usage)
     parser.add_option('-k', type='int', dest='k', default=10,
         help='Number of predicted edges per node.', metavar='NUM')
     parser.add_option('-s', '--savefile', dest='savefile',
         default='predictEdges.pickle', help='Pickle to dump predicted edges.',
-        metavar='FILE')
-    parser.add_option('--tfidf', action='store_true', dest='tfidf_compare',
-        default=False, help='Make predictions based on item-item tfidf similarity.')
-    parser.add_option('-d', '--database', dest='dbname',
-        default='data/macys.db', help='Database to pull descriptions from.')
-    parser.add_option('-i', '--idfname', dest='idfname',
-        help='Name of pickle with saved idfs', metavar='FILE')
-    parser.add_option('--stopwords', dest='stopwords',
-        default='data/stopwords.txt',
-        help='File containing a comma separated list of stop words.',
         metavar='FILE')
     return parser
 
@@ -81,63 +61,6 @@ def predictEdges(topic_space, dictionary, k, searchEngine):
                             for neighbor in neighbors[0]]
     return predicted_edges
 
-def tfidfSimilarity(tfidf1, tfidf2):
-    score = 0.0
-    weight1 = 0.0
-    weight2 = 0.0
-    for word2 in tfidf2:
-        weight2 += tfidf2[word2]*tfidf2[word2] 
-    for word1 in tfidf1:
-        weight1 += tfidf1[word1]*tfidf1[word1]
-        if word1 in tfidf2:
-            score += tfidf1[word1]*tfidf2[word1]
-    return score
-    # for cosine similarity:
-    #return (score / math.sqrt(weight1 * weight2))
-
-def tfidfNeighbors(tfidf1, tfidfs2, k):
-    queue = PriorityQueue() # priority queue makes this nlogk
-    for node2 in tfidfs2:
-        similarity = tfidfSimilarity(tfidf1, tfidfs2[node2])
-        queue.put((similarity, node2))
-        if queue.qsize() > k:
-            queue.get()
-    neighbors = []
-    while not queue.empty():
-        (similarity, node2) = queue.get()
-        neighbors.append(node2)
-    return neighbors
-
-def calculateTfidfs(db_conn, dictionary, idf, stopwords=None):
-    db_curs = db_conn.cursor()
-    tfidfs = {}
-    for item in dictionary:
-        tfidf = defaultdict(float)
-        db_curs.execute(selectDescriptionStmt, (dictionary[item],))
-        description = db_curs.fetchone()[0]
-        description = ''.join(ch for ch in description\
-                              if ch not in string.punctuation)
-        words = [stem(w.lower()) for w in description.split()]
-        for word in words:
-            if stopwords is not None and word in stopwords:
-                continue
-            tfidf[word] += idf[word]
-        tfidfs[dictionary[item]] = tfidf
-    return tfidfs
-
-def predictTfidfEdges(db_conn, dictionary1, dictionary2, k, idf, stopwords=None):
-    predicted_edges = []
-    tfidfs1 = calculateTfidfs(db_conn, dictionary1, idf, stopwords=stopwords)
-    tfidfs2 = calculateTfidfs(db_conn, dictionary2, idf, stopwords=stopwords)
-    count = 0
-    for node1 in tfidfs1:
-        if count % displayInterval == 0:
-            print 'Getting neighbors of %d / %d nodes' % (count, len(tfidfs1.keys()))
-        count += 1
-        neighbors = tfidfNeighbors(tfidfs1[node1], tfidfs2, k)
-        predicted_edges += [(node1, n) for n in neighbors]
-    return predicted_edges
-
 def main():
     # Parse options
     usage = 'Usage: %prog [options] topicMap.pickle modelfile1 modelfile2'
@@ -173,31 +96,8 @@ def main():
     print 'Creating KNN search engine from model2. . .'
     searchEngine = KNNSearchEngine(data2.transpose(), dictionary2)
 
-    if options.tfidf_compare:
-        print 'Predicting edges by item-item tfidf similarity. .'
-        print 'Loading Stopwords and IDFs. . .'
-        # get stop words
-        if os.path.isfile(options.stopwords):
-            with open(options.stopwords, 'r') as f:
-                try:
-                    stopwords = f.readline().split(',')
-                except:
-                    print >> sys.stderr, 'Failed to parse stop words.'
-                    return
-        else:
-            print >> sys.stderr,\
-                'WARNING: stop words file not found: %s' % options.stopwords
-            stopwords = None
-
-        with open(options.idfname, 'r') as f:
-            idf = pickle.load(f)
-        print 'Connecting to database. .'
-        db_conn = sqlite3.connect(options.dbname)
-        predicted_edges = predictTfidfEdges(db_conn, dictionary1, dictionary2,
-                                            options.k, idf, stopwords=stopwords)
-    else:
-        print 'Predicting edges. . .'
-        predicted_edges = predictEdges(transformed_space, dictionary1,
+    print 'Predicting edges. . .'
+    predicted_edges = predictEdges(transformed_space, dictionary1,
                                        options.k, searchEngine)
     
     # save results
