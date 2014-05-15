5f5509f03197c94cbc26f9c66c00a6de643f06ba
Bill Chickering <chickering@cs.stanford.edu>
2014-05-15 12:48:01 -0700

    Amend matching/polls/models.py.

--- matching/polls/models.py
+++ matching/polls/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    shortname = models.CharField(max_length=20)
+    longname = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.shortname
+
+class Product(models.Model):
+    company = models.ForeignKey(Company)
+    item_id = models.IntegerField()
+    url = models.TextField()
+    description = models.TextField()
+    def __unicode__(self):
+        return self.description
+
+class Poll(models.Model):
+    product = models.ForeignKey(Product)
+    pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.product
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    product = models.ForeignKey(Product)
+    votes = models.IntegerField(default=0)
+    def __unicode__(self):
+        return self.product
+    
