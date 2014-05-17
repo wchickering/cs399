1609a7ac95423afe82eb8b5a777a79fbd3caa331
Bill Chickering <chickering@cs.stanford.edu>
2014-05-17 16:10:05 -0700

    Add clean() methods to tourneys models.

--- matching/tourneys/models.py
+++ matching/tourneys/models.py
@@ -3,6 +3,7 @@ import os
 
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 # analogous to a retailer
 class League(models.Model):
@@ -43,6 +44,14 @@ class PlayerAttribute(models.Model):
         return '%s = %0.2e' % (unicode(self.attribute), self.value)
     def league(self):
         return self.attribute.league
+    def clean(self):
+        if self.player.league != self.attribute.league:
+            raise ValidationError(
+                ('Player League (%(playerleague)s) not equal to Attribute '
+                 'League (%(attributeleague)s)'),
+                params={'playerleague': self.player.league,
+                        'attributeleague': self.attribute.league}
+            )
 
 # an attribute (i.e. concept) in the context of tournaments
 class Team(models.Model):
@@ -65,6 +74,14 @@ class TeamPlayer(models.Model):
         return self.player.image_tag()
     image_tag.short_description = 'Image'
     image_tag.allow_tags = True
+    def clearn(self):
+        if self.team.attribute.league != self.player.league:
+            raise ValidationError(
+                ('Team League (%(teamleague)s) not equal to Player '
+                 'League (%(playerleague)s)'),
+                params={'teamleague': self.team.attribute.league,
+                        'playerleague': self.player.league}
+            )
 
 # analogous to a MTurk job
 class Tournament(models.Model):
@@ -80,6 +97,15 @@ class Tournament(models.Model):
         return self.team.attribute
     def targetleague(self):
         return self.targetattribute().league
+    def clean(self):
+        if self.round == 0:
+            raise ValidationError('Round must be finite.')
+        if self.league == self.team.attribute.league:
+            raise ValidationError(
+                ('Source League (%(league)s) is equal to Target Team '
+                 'League'),
+                params={'league': self.league}
+            )
 
 # a competition between multiple source concepts to match a target concept
 class Competition(models.Model):
@@ -95,6 +121,15 @@ class Competition(models.Model):
         return self.tournament.team
     def targetleague(self):
         return self.tournament.targetleague()
+    def clean(self):
+        if self.next_competition is not None:
+            if self.next_competition.tournament != self.tournament:
+                raise ValidationError(
+                    ('Next competition tournament (%(nexttournary)s) not equal '
+                     'to this competition tournament (%(thistourney)s)'),
+                    params={'nexttourney': self.next_competition.tournament,
+                            'thistourney': self.tournament}
+                )
 
 # a team (i.e. concept) in the context of a competition
 class CompetitionTeam(models.Model):
@@ -103,13 +138,30 @@ class CompetitionTeam(models.Model):
     score = models.FloatField(null=True, blank=True)
     def __unicode__(self):
         return unicode(self.team)
+    def clean(self):
+        if self.competition.tournament.league != self.team.attribute.league:
+            raise ValidationError(
+                ('Competition League (%(competitionleague)s) not equal to '
+                 'Team League (%(teamleague)s)'),
+                params={'competitionleague': self.competition.tournament.league,
+                        'teamleague': self.team.attribute.league}
+            )
 
 # analogous to a task in MTurk
 # a competition consists of several matches
 class Match(models.Model):
     competition = models.ForeignKey(Competition)
+    teamplayer = models.ForeignKey(TeamPlayer) # target team player
     def __unicode__(self):
-        return unicode(self.competition)
+        return unicode(self.teamplayer)
+    def clean(self):
+        if self.competition.tournament.team != self.teamplayer.team:
+            raise ValidationError(
+                ('Competition Team (%(competitionteam)s) not equal to '
+                 'TeamPlayer Team (%(playerteam)s)'),
+                params={'competitionteam': self.competition.tournament.team,
+                        'playerteam': self.teamplayer.team}
+            )
 
 # a player (i.e. product) in the context of a match
 class Competitor(models.Model):
@@ -119,4 +171,19 @@ class Competitor(models.Model):
     winner = models.NullBooleanField()
     def __unicode__(self):
         return '%s (%s)' % (unicode(self.player), unicode(self.team))
+    def clean(self):
+        if self.match.competition != self.competitionteam.competition:
+            raise ValidationError(
+                ('Match Competition (%(matchcompetition)s) not equal to '
+                 'CompetitionTeam Competition (%(teamcompetition)s)'),
+                params={'matchcompetition': self.match.competition,
+                        'teamcompetition': self.competitionteam.competition}
+            )
+        if self.teamplayer.team != self.competitionteam.team:
+            raise ValidationError(
+                ('TeamPlayer Team (%(playerteam)s) not equal to '
+                 'CompetitionTeam Team (%(teamteam)s)'),
+                params={'playerteam': self.teamplayer.team,
+                        'teamteam': self.competitionteam.team}
+            )
     
