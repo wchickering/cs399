e5963011f875af49d1c48101e19917a412c13582
Bill Chickering <chickering@cs.stanford.edu>
2014-05-17 21:14:43 -0700

    Refactor cleanmodel command into AppCommand.

--- matching/tourneys/management/commands/cleanmodel.py
+++ matching/tourneys/management/commands/cleanmodel.py
@@ -1,9 +1,21 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.db.models import get_app, get_models
+"""
+Invoke .full_clean() on all models for one or more given apps.
+"""
+
+from optparse import make_option
+
+from django.core.management.base import AppCommand
+from django.db.models import get_models
 from django.core.exceptions import ValidationError
 
-class Command(BaseCommand):
+class Command(AppCommand):
     help = 'Validate consistency of tourneys model'
+    option_list = AppCommand.option_list + (
+        make_option('--exclude',
+            dest='exclude',
+            default=None,
+            help='Exclude model(s) from cleaning.'),
+        )
 
     def writeError(self, model, instance, error):
         self.stderr.write(
@@ -11,13 +23,16 @@ class Command(BaseCommand):
             (model.__name__, instance.pk, '\n\t'.join(error.messages))
         )
 
-    def handle(self, *args, **options):
-        app = get_app(args[0])
+    def handle_app(self, app, **options):
+        exclude_list = []
+        if 'exclude' in options and options['exclude'] is not None:
+            exclude_list += options['exclude'].split(',')
         for model in get_models(app):
+            if model.__name__ in exclude_list: continue
             self.stdout.write('Cleaning %s. . .' % model.__name__)
             for instance in model.objects.all():
                 try:
                     instance.full_clean()
                 except ValidationError as e:
                     self.writeError(model, instance, e)
-                    break
+                    return
