a119cbad4b3464b1618c27fac9f24009b85a9f8f
Jamie Irvine <jirvine@cs.stanford.edu>
2014-05-26 19:38:35 -0700

    Remove normalization from mapTopics = big boost in performance

--- common/mapTopics.py
+++ common/mapTopics.py
@@ -23,9 +23,6 @@ def getParser(usage=None):
     parser.add_option('--savefile', dest='savefile',
         default='data/topic_map.pickle',
         help='Name of pickle to write topic map to.', metavar='FILE')
-    parser.add_option('-n', '--topnwords', type=int, dest='topnwords', 
-        default=1000, help='Number of top words per topic to compare.',
-        metavar='NUM')
     parser.add_option('-v', '--verbose', action='store_true', dest='verbose',
         default=False, help='Print top words')
     parser.add_option('-d', '--by_distance', action='store_true',
@@ -37,20 +34,9 @@ def getParser(usage=None):
 
 def topicSimilarity(topic1, topic2):
     similarity = 0.0
-    # Traverse ordered lists of words for matches
-    i = 0
-    j = 0
-    while i < len(topic1) and j < len(topic2):
-        word1 = topic1[i][0]
-        word2 = topic2[j][0]
-        if word1 == word2:
-            similarity += float(topic1[i][1]) * float(topic2[j][1])
-            i += 1
-            j += 1
-        elif word1 < word2:
-            i += 1
-        else:
-            j += 1
+    for word in topic1:
+        if word in topic2:
+            similarity += float(topic1[word])*float(topic2[word])
     return similarity
 
 def topicDistance(topic1, topic2):
@@ -68,14 +54,6 @@ def topicDistance(topic1, topic2):
             sqr_distance += p2*p2
     return math.sqrt(sqr_distance)
 
-def transformMatrix(matrix):
-    matrix_t = [[0 for x in xrange(len(matrix))]\
-                for x in xrange(len(matrix[0]))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            matrix_t[j][i] = matrix[i][j]
-    return matrix_t
-
 def getTopicMap(topics1, topics2, max_connections, by_distance):
     topic_map = [] 
     for topic1 in topics1:
@@ -84,7 +62,7 @@ def getTopicMap(topics1, topics2, max_connections, by_distance):
             if by_distance:
                 similarity = 1.0/topicDistance(dict(topic1), dict(topic2))
             else:
-                similarity = topicSimilarity(topic1, topic2)
+                similarity = topicSimilarity(dict(topic1), dict(topic2))
             topic_vector.append(similarity)
         # if all similarities zero, then set all similarities to 1.0
         if np.linalg.norm(topic_vector, 1) == 0.0:
@@ -97,14 +75,9 @@ def getTopicMap(topics1, topics2, max_connections, by_distance):
             if queue.qsize() > max_connections:
                 (similarity, idx) = queue.get()
                 topic_vector[idx] = 0.0
-        topic_map.append(topic_vector/np.linalg.norm(topic_vector, 1))
-    return transformMatrix(topic_map)
-
-def truncateTopics(topics, topnwords):
-    for topic in range(len(topics)):
-        topics[topic] = np.array(topics[topic])
-        topics[topic] = topics[topic][0:topnwords, :]
-    return topics
+        #topic_map.append(topic_vector/np.linalg.norm(topic_vector, 0))
+        topic_map.append(topic_vector)
+    return np.array(topic_map).transpose()
 
 def main():
     # Parse options
@@ -124,27 +97,10 @@ def main():
     print 'Loading topics of first catalogue from %s. . .' % topics2_fname
     topics2 = loadPickle(topics2_fname)
 
-    # truncate to get top words in each topic
-    topics1 = truncateTopics(topics1, options.topnwords)
-    topics2 = truncateTopics(topics2, options.topnwords)
-
-    # sort each topic by word alphabetically
-    print 'Sorting topics by word. . .'
-    for topic in range(len(topics1)):
-        ind = np.argsort(topics1[topic][:,0])
-        topics1[topic] = topics1[topic][ind, :]
-    for topic in range(len(topics2)):
-        ind = np.argsort(topics2[topic][:,0])
-        topics2[topic] = topics2[topic][ind, :]
-        
     # get matrix mapping topics from space 1 to topics of space 2
     print 'Mapping topics between catalogues. . .'
     topic_map = getTopicMap(topics1, topics2, options.max_connections,
             options.by_distance)
-
-    # assert that all columns add up to 1
-    assert(np.array(topic_map).sum(axis=0).all() == 
-            np.ones((len(topic_map), 1)).all())
 
     # dump tf-idfs
     print 'Saving topic map to %s. . .' % options.savefile
