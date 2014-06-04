a1a70bbad978c1a4f8938ea3fe182240805a1b65
Bill Chickering <chickering@cs.stanford.edu>
2014-06-04 12:28:33 -0700

    fix divide-by-zero bug and change names to camel case in evalPredictedEdges.py.

--- common/evalPredictedEdges.py
+++ common/evalPredictedEdges.py
@@ -31,74 +31,78 @@ def getDict(filename):
     id2item = npzfile['dictionary']
     return dict((id2item[i], i) for i in range(len(id2item)))
 
-def evalEdges(edges, prox_mat, dictionary, k):
+def evalEdges(edges, proxMat, dictionary, k):
     correct = 0
     eps = 0.0001
-    key_errors = 0
+    keyErrors = 0
     for item1, item2 in edges:
         try:
             id1 = dictionary[item1]
             id2 = dictionary[item2]
-            proximity = prox_mat[id1, id2]
+            proximity = proxMat[id1, id2]
             if proximity != 0 and -1*math.log(proximity) < (k + eps):
                 correct += 1
         except KeyError:
-            key_errors += 1
+            keyErrors += 1
             pass
-    if key_errors > 0:
+    if keyErrors > 0:
         print >> sys.stderr,\
             ('WARNING: %d/%d predicted (lost) edges with one or both nodes '
              'missing from target (source) graph.') % (
-                key_errors,
+                keyErrors,
                 len(edges)
             )
     return correct
 
 def main():
     # Parse options
-    usage = ('Usage: %prog [options] target_prox_mat.npz source_prox_mat.npz '
-             'lost_edges.pickle predicted_edges.pickle')
+    usage = ('Usage: %prog [options] targetProxMat.npz sourceProxMat.npz '
+             'lostEdges.pickle predictedEdges.pickle')
     parser = getParser(usage=usage)
     (options, args) = parser.parse_args()
     if len(args) != 4:
         parser.error('Wrong number of arguments')
-    target_prox_mat_fname = getAndCheckFilename(args[0])
-    source_prox_mat_fname = getAndCheckFilename(args[1])
-    lost_edges_filename = getAndCheckFilename(args[2])
-    predicted_edges_filename = getAndCheckFilename(args[3])
+    targetProxMatFilename = getAndCheckFilename(args[0])
+    sourceProxMatFilename = getAndCheckFilename(args[1])
+    lostEdgesFilename = getAndCheckFilename(args[2])
+    predictedEdgesFilename = getAndCheckFilename(args[3])
 
     # Load edges
-    target_prox_mat = getMatrix(target_prox_mat_fname)
-    target_dict = getDict(target_prox_mat_fname)
-    source_prox_mat = getMatrix(source_prox_mat_fname)
-    source_dict = getDict(source_prox_mat_fname)
-    lost_edges = loadPickle(lost_edges_filename)
-    predicted_edges = loadPickle(predicted_edges_filename)
+    targetProxMat = getMatrix(targetProxMatFilename)
+    targetDict = getDict(targetProxMatFilename)
+    sourceProxMat = getMatrix(sourceProxMatFilename)
+    sourceDict = getDict(sourceProxMatFilename)
+    lostEdges = loadPickle(lostEdgesFilename)
+    predictedEdges = loadPickle(predictedEdgesFilename)
 
     # Evaluate
-    correct_predictions = evalEdges(predicted_edges, target_prox_mat,
-                                    target_dict, options.k)
-    recalled_edges = evalEdges(lost_edges, source_prox_mat,
-                               source_dict, options.k)
+    correctPredictions = evalEdges(predictedEdges, targetProxMat,
+                                    targetDict, options.k)
+    recalledEdges = evalEdges(lostEdges, sourceProxMat,
+                               sourceDict, options.k)
+
+    # get results
+    numCorrectPredictions = len(predictedEdges)
+    numLostEdges = len(lostEdges)
+    if not options.directed:
+        recalledEdges /= 2
+        numLostEdges /= 2
 
     # compute metrics
-    num_correct_predictions = len(predicted_edges)
-    num_lost_edges = len(lost_edges)
-    if not options.directed:
-        recalled_edges /= 2
-        num_lost_edges /= 2
+    precision = float(correctPredictions) / numCorrectPredictions
+    recall = float(recalledEdges) / numLostEdges
+    if precision == 0.0 and recall == 0.0:
+        f1score = 0.0
+    else:
+        f1score = 2*precision*recall/(precision + recall)
 
-    precision = float(correct_predictions) / num_correct_predictions
-    recall = float(recalled_edges) / num_lost_edges
-    f1score = 2*precision*recall/(precision + recall)
-
-    # print evaluation results
+    # display results
     print '==============================================='
-    print 'Correct predictions \t : %d' % correct_predictions
-    print 'Total predictions \t : %d' % len(predicted_edges)
+    print 'Correct predictions \t : %d' % correctPredictions
+    print 'Total predictions \t : %d' % len(predictedEdges)
     print '%d-Precision \t\t : %0.3f' % (options.k, precision)
-    print 'Recalled Edges (k=%d)\t : %d' % (options.k, recalled_edges)
-    print 'Withheld edges \t\t : %d' % num_lost_edges
+    print 'Recalled Edges (k=%d)\t : %d' % (options.k, recalledEdges)
+    print 'Withheld edges \t\t : %d' % numLostEdges
     print '%d-Recall \t\t : %0.3f' % (options.k, recall)
     print '%d-F1 Score \t\t : %0.3f' % (options.k, f1score)
 
