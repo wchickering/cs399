ebce3e4225d04b974ec2710f9e96fe1f560e35d2
Bill Chickering <chickering@cs.stanford.edu>
2014-06-10 10:23:14 -0700

    Add --delim option and help to joinEveryNLines.sh.

--- common/joinEveryNLines.sh
+++ common/joinEveryNLines.sh
@@ -9,10 +9,38 @@ verify_number () {
     fi
 }
 
+COMMAND=${0##*/}
+
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            echo "$COMMAND - join every N lines"
+            echo
+            echo "Usage: $COMMAND [options] N file"
+            echo
+            echo "options:"
+            echo "-h, --help                Show brief help"
+            echo "--delim=STRING            Delimiter for joined lines"
+            exit 0
+            ;;
+        --delim*)
+            DELIM=`echo $1 | sed -e 's/^[^=]*=//g'`
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if [ $# -lt 2 ]; then
+    die "Usage: $COMMAND [options] N file"
+fi
+
 # get args
 N=$1
 FILE=$2
 
 verify_number $N
 
-perl -pi -e "s/\r\n/ / if $.%$N" $FILE 2> /dev/null
+perl -pi -e "s/\r\n/$DELIM/ if $.%$N" $FILE 2> /dev/null
